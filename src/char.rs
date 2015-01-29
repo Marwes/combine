176c0d3f31f7cc099dac53b82bb173d8ef68e632
jxv <jxv@hush.com>
2015-01-29 15:45:22 -0800

    add some basic character parsers

--- src/char.rs
+++ src/char.rs
@@ -1,5 +1,5 @@
 use primitives::{Consumed, Parser, ParseError, ParseResult, Error, State, Stream};
-use combinator::{FnParser, many, Many, Map, ParserExt};
+use combinator::{FnParser, many, Many, Map, ParserExt, With};
 
 macro_rules! impl_char_parser {
     ($name: ident ($($ty_var: ident),*), $inner_type: ty) => {
@@ -93,6 +93,71 @@ pub fn spaces<I>() -> Spaces<I>
     where I: Stream<Item=char> {
     Spaces(many(space().map(static_fn!((_, char) -> () { () }))))
 }
+
+impl_char_parser! { NewLine(), Satisfy<I, fn (char) -> bool> }
+///Parses a newline character
+pub fn newline<I>() -> NewLine<I>
+    where I: Stream<Item=char> {
+    NewLine(satisfy(static_fn!((ch, char) -> bool { ch == '\n' })))
+}
+
+impl_char_parser! { CrLf(), With<Satisfy<I, fn (char) -> bool>, NewLine<I>> }
+///Parses carriage return and newline, returning the newline character.
+pub fn crlf<I>() -> CrLf<I>
+    where I: Stream<Item=char> {
+    CrLf(satisfy(static_fn!((ch, char) -> bool { ch == '\r' }) as fn (char) -> bool)
+        .with(newline()))
+}
+
+impl_char_parser! { Tab(), Satisfy<I, fn (char) -> bool> }
+///Parses a tab character
+pub fn tab<I>() -> Tab<I>
+    where I: Stream<Item=char> {
+    Tab(satisfy(static_fn!((ch, char) -> bool { ch == '\t' })))
+}
+
+impl_char_parser! { Upper(), Satisfy<I, fn (char) -> bool> }
+///Parses an uppercase letter
+pub fn upper<I>() -> Upper<I>
+    where I: Stream<Item=char> {
+    Upper(satisfy(static_fn!((ch, char) -> bool { ch.is_uppercase()})))
+}
+
+impl_char_parser! { Lower(), Satisfy<I, fn (char) -> bool> }
+///Parses an uppercase letter
+pub fn lower<I>() -> Lower<I>
+    where I: Stream<Item=char> {
+    Lower(satisfy(static_fn!((ch, char) -> bool { ch.is_lowercase() })))
+}
+
+impl_char_parser! { AlphaNum(), Satisfy<I, fn (char) -> bool> }
+///Parses either an alphabet letter or digit
+pub fn alpha_num<I>() -> AlphaNum<I>
+    where I: Stream<Item=char> {
+    AlphaNum(satisfy(static_fn!((ch, char) -> bool { ch.is_alphanumeric() })))
+}
+
+impl_char_parser! { Letter(), Satisfy<I, fn (char) -> bool> }
+///Parses an alphabet letter
+pub fn letter<I>() -> Letter<I>
+    where I: Stream<Item=char> {
+    Letter(satisfy(static_fn!((ch, char) -> bool { ch.is_alphabetic() })))
+}
+
+impl_char_parser! { OctDigit(), Satisfy<I, fn (char) -> bool> }
+///Parses an octal digit
+pub fn oct_digit<I>() -> OctDigit<I>
+    where I: Stream<Item=char> {
+    OctDigit(satisfy(static_fn!((ch, char) -> bool { ch.is_digit(8) })))
+}
+
+impl_char_parser! { HexDigit(), Satisfy<I, fn (char) -> bool> }
+///Parses a hexdecimal digit with uppercase and lowercase
+pub fn hex_digit<I>() -> HexDigit<I>
+    where I: Stream<Item=char> {
+    HexDigit(satisfy(static_fn!((ch, char) -> bool { ch.is_digit(0x10) })))
+}
+
 
 #[derive(Clone)]
 pub struct StringP<'a, I> { s: &'a str }
