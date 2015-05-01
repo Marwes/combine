6222a6bdacc26f2682f0981815eef91c13b8abfd
Markus <marwes91@gmail.com>
2015-05-01 11:46:15 +0200

    Generalized the json parser to work with an type of input

--- benches/json.rs
+++ benches/json.rs
@@ -7,7 +7,7 @@ use std::io::Read;
 use std::fs::File;
 use std::path::Path;
 
-use pc::primitives::{Parser, State, ParseResult};
+use pc::primitives::{Parser, State, Stream, ParseResult};
 use pc::combinator::{between, many, many1, optional, parser, sep_by, unexpected, With, ParserExt};
 use pc::char::{any_char, digit, satisfy, spaces, Spaces, string};
 
@@ -21,111 +21,120 @@ enum Value {
     Array(Vec<Value>)
 }
 
-fn lex<'a, P>(p: P) -> With<Spaces<&'a str>, P>
-    where P: Parser<Input=&'a str> {
+fn lex<'a, P>(p: P) -> With<Spaces<P::Input>, P>
+    where P: Parser
+        , P::Input: Stream<Item=char> {
     spaces().with(p)
 }
+struct Json<I>(::std::marker::PhantomData<fn (I) -> I>);
 
-fn integer(input: State<&str>) -> ParseResult<i64, &str> {
-    let (s, input) = try!(many1::<String, _>(digit())
-        .expected("integer")
-        .parse_state(input));
-    let mut n = 0;
-    for c in s.chars() {
-        n = n * 10 + (c as i64 - '0' as i64);
+impl <I> Json<I>
+    where I: Stream<Item=char> {
+
+    fn integer(input: State<I>) -> ParseResult<i64, I> {
+        let (s, input) = try!(many1::<String, _>(digit())
+            .expected("integer")
+            .parse_state(input));
+        let mut n = 0;
+        for c in s.chars() {
+            n = n * 10 + (c as i64 - '0' as i64);
+        }
+        Ok((n, input))
     }
-    Ok((n, input))
-}
 
-fn number(input: State<&str>) -> ParseResult<f64, &str> {
-    let i = satisfy(|c| c == '0').map(|_| 0.0)
-             .or(parser(integer).map(|x| x as f64));
-    let fractional = many(digit())
-        .map(|digits: String| {
-            let mut magnitude = 1.0;
-            digits.chars().fold(0.0, |acc, d| {
-                magnitude /= 10.0;
-                match d.to_digit(10) {
-                    Some(d) => acc + (d as f64) * magnitude,
-                    None => panic!("Not a digit")
+    fn number(input: State<I>) -> ParseResult<f64, I> {
+        let i = satisfy(|c| c == '0').map(|_| 0.0)
+                 .or(parser(Json::<I>::integer).map(|x| x as f64));
+        let fractional = many(digit())
+            .map(|digits: String| {
+                let mut magnitude = 1.0;
+                digits.chars().fold(0.0, |acc, d| {
+                    magnitude /= 10.0;
+                    match d.to_digit(10) {
+                        Some(d) => acc + (d as f64) * magnitude,
+                        None => panic!("Not a digit")
+                    }
+                })
+            });
+
+        let exp = satisfy(|c| c == 'e' || c == 'E')
+            .with(optional(string("-")).and(parser(Json::<I>::integer)));
+        optional(string("-"))
+            .and(i)
+            .map(|(sign, n)| if sign.is_some() { -n } else { n })
+            .and(optional(string(".")).with(fractional))
+            .map(|(x, y)| if x > 0.0 { x + y } else { x - y })
+            .and(optional(exp))
+            .map(|(n, exp_option)| {
+                match exp_option {
+                    Some((sign, e)) => {
+                        let e = if sign.is_some() { -e } else { e };
+                        n * 10.0f64.powi(e as i32)
+                    }
+                    None => n
                 }
             })
-        });
-
-    let exp = satisfy(|c| c == 'e' || c == 'E')
-        .with(optional(string("-")).and(parser(integer)));
-    optional(string("-"))
-        .and(i)
-        .map(|(sign, n)| if sign.is_some() { -n } else { n })
-        .and(optional(string(".")).with(fractional))
-        .map(|(x, y)| if x > 0.0 { x + y } else { x - y })
-        .and(optional(exp))
-        .map(|(n, exp_option)| {
-            match exp_option {
-                Some((sign, e)) => {
-                    let e = if sign.is_some() { -e } else { e };
-                    n * 10.0f64.powi(e as i32)
-                }
-                None => n
-            }
-        })
-    .parse_state(input)
-}
-
-fn json_char(input: State<&str>) -> ParseResult<char, &str> {
-    let (c, input) = try!(parser(any_char).parse_state(input));
-    let mut back_slash_char = satisfy(|c| "\"\\/bfnrt".chars().find(|x| *x == c).is_some()).map(|c| {
-        match c {
-            '"' => '"',
-            '\\' => '\\',
-            '/' => '/',
-            'b' => '\u{0008}',
-            'f' => '\u{000c}',
-            'n' => '\n',
-            'r' => '\r',
-            't' => '\t',
-            c => c//Should never happen
-        }
-    });
-    match c {
-        '\\' => input.combine(|input| back_slash_char.parse_state(input)),
-        '"'  => unexpected("\"").parse_state(input.into_inner()).map(|_| unreachable!()),
-        _    => Ok((c, input))
+        .parse_state(input)
     }
-}
-fn json_string(input: State<&str>) -> ParseResult<String, &str> {
-    between(string("\""), string("\""), many(parser(json_char)))
-        .parse_state(input)
-}
-fn object(input: State<&str>) -> ParseResult<Value, &str> {
-    let field = lex(parser(json_string))
-        .skip(lex(string(":")))
-        .and(lex(parser(json_value)));
-    let fields = sep_by(field, string(","));
-    between(string("{"), lex(string("}")), fields)
-        .map(Value::Object)
-        .parse_state(input)
-}
 
-#[allow(unconditional_recursion)]
-fn json_value(input: State<&str>) -> ParseResult<Value, &str> {
-    let array = between(string("["), lex(string("]")), sep_by(parser(json_value), string(",")))
-        .map(Value::Array);
-
-    //Wrap a few of the value parsers to workaround the slow compiletimes
-    fn rest(input: State<&str>) -> ParseResult<Value, &str> {
-        string("false").map(|_| Value::Bool(false))
-            .or(string("true").map(|_| Value::Bool(true)))
-            .or(string("null").map(|_| Value::Null))
+    fn char(input: State<I>) -> ParseResult<char, I> {
+        let (c, input) = try!(parser(any_char).parse_state(input));
+        let mut back_slash_char = satisfy(|c| "\"\\/bfnrt".chars().find(|x| *x == c).is_some()).map(|c| {
+            match c {
+                '"' => '"',
+                '\\' => '\\',
+                '/' => '/',
+                'b' => '\u{0008}',
+                'f' => '\u{000c}',
+                'n' => '\n',
+                'r' => '\r',
+                't' => '\t',
+                c => c//Should never happen
+            }
+        });
+        match c {
+            '\\' => input.combine(|input| back_slash_char.parse_state(input)),
+            '"'  => unexpected("\"").parse_state(input.into_inner()).map(|_| unreachable!()),
+            _    => Ok((c, input))
+        }
+    }
+    fn string(input: State<I>) -> ParseResult<String, I> {
+        between(string("\""), string("\""), many(parser(Json::<I>::char)))
             .parse_state(input)
     }
-    lex(parser(json_string).map(Value::String)
-        .or(parser(object))
-        .or(array)
-        .or(parser(number).map(Value::Number))
-        .or(parser(rest))
-    )
-        .parse_state(input)
+    fn object(input: State<I>) -> ParseResult<Value, I> {
+        let field = lex(parser(Json::<I>::string))
+            .skip(lex(string(":")))
+            .and(lex(parser(Json::<I>::value)));
+        let fields = sep_by(field, string(","));
+        between(string("{"), lex(string("}")), fields)
+            .map(Value::Object)
+            .parse_state(input)
+    }
+
+    #[allow(unconditional_recursion)]
+    fn value(input: State<I>) -> ParseResult<Value, I>
+        where I: Stream<Item=char> {
+        let array = between(string("["), lex(string("]")), sep_by(parser(Json::<I>::value), string(",")))
+            .map(Value::Array);
+
+        //Wrap a few of the value parsers to workaround the slow compiletimes
+        fn rest<I>(input: State<I>) -> ParseResult<Value, I>
+            where I: Stream<Item=char> {
+            string("false").map(|_| Value::Bool(false))
+                .or(string("true").map(|_| Value::Bool(true)))
+                .or(string("null").map(|_| Value::Null))
+                .parse_state(input)
+        }
+        lex(parser(Json::<I>::string).map(Value::String)
+            .or(parser(Json::<I>::object))
+            .or(array)
+            .or(parser(Json::<I>::number).map(Value::Number))
+            .or(parser(rest))
+        )
+            .parse_state(input)
+    }
+
 }
 
 #[test]
@@ -144,7 +153,7 @@ r#"
     "false"  : false,
     "null" : null
 }"#;
-    let result = parser(json_value)
+    let result = parser(Json::value)
         .parse(input);
     let expected = Object(vec![
         ("array", Array(vec![Number(1.0), String("".to_string())])),
@@ -172,7 +181,7 @@ fn bench_json(bencher: &mut ::test::Bencher) {
     File::open(&Path::new(&"benches/data.json"))
         .and_then(|mut file| file.read_to_string(&mut data))
         .unwrap();
-    let mut parser = parser(json_value);
+    let mut parser = parser(Json::value);
     match parser.parse(&data[..]) {
         Ok((Value::Array(_), "\r\n")) => (),
         Ok(x) => { println!("{:?}", x); assert!(false); }
