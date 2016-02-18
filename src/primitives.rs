f21148b3c4c5c6f10d8b6d90ce4a7925596879b3
Markus Westerlind <marwes91@gmail.com>
2016-02-18 21:22:57 +0100

    fix: Fix the position handling of BufferedStream

--- src/primitives.rs
+++ src/primitives.rs
@@ -979,21 +979,24 @@ impl<I, O, P: ?Sized> Parser for Box<P>
 
 #[cfg(feature = "buffered_stream")]
 pub struct BufferedStream<'a, I>
-    where I: Iterator + 'a,
+    where I: Iterator + HasPosition + 'a,
           I::Item: 'a
 {
     offset: usize,
+    position: I::Position,
     buffer: &'a SharedBufferedStream<I>,
 }
 
 #[cfg(feature = "buffered_stream")]
 impl<'a, I> Clone for BufferedStream<'a, I>
-    where I: Iterator + 'a,
+    where I: Iterator + HasPosition + 'a,
+          I::Position: Clone,
           I::Item: 'a
 {
     fn clone(&self) -> BufferedStream<'a, I> {
         BufferedStream {
             offset: self.offset,
+            position: self.position.clone(),
             buffer: self.buffer,
         }
     }
@@ -1001,26 +1004,27 @@ impl<'a, I> Clone for BufferedStream<'a, I>
 
 #[cfg(feature = "buffered_stream")]
 pub struct SharedBufferedStream<I>
-    where I: Iterator
+    where I: Iterator + HasPosition
 {
     buffer: UnsafeCell<BufferedStreamInner<I>>,
 }
 
 #[cfg(feature = "buffered_stream")]
 struct BufferedStreamInner<I>
-    where I: Iterator
+    where I: Iterator + HasPosition
 {
     offset: usize,
     iter: I,
-    buffer: VecDeque<I::Item>,
+    buffer: VecDeque<(I::Item, I::Position)>,
 }
 
 #[cfg(feature = "buffered_stream")]
 impl<I> BufferedStreamInner<I>
-    where I: Iterator,
+    where I: Iterator + HasPosition,
+          I::Position: Clone,
           I::Item: Clone
 {
-    fn uncons(&mut self, offset: usize) -> Result<I::Item, Error<I::Item, I::Item>> {
+    fn uncons(&mut self, offset: usize) -> Result<(I::Item, I::Position), Error<I::Item, I::Item>> {
         if offset >= self.offset {
             let item = match self.iter.next() {
                 Some(item) => item,
@@ -1032,8 +1036,9 @@ impl<I> BufferedStreamInner<I>
             if self.buffer.len() == self.buffer.capacity() {
                 self.buffer.pop_front();
             }
-            self.buffer.push_back(item.clone());
-            Ok(item)
+            let position = self.iter.position();
+            self.buffer.push_back((item.clone(), position.clone()));
+            Ok((item, position))
         } else if offset < self.offset - self.buffer.len() {
             // We have backtracked to far
             Err(Error::Message("Backtracked to far".into()))
@@ -1045,22 +1050,24 @@ impl<I> BufferedStreamInner<I>
 
 #[cfg(feature = "buffered_stream")]
 impl<I> SharedBufferedStream<I>
-    where I: Iterator,
+    where I: Iterator + HasPosition,
+          I::Position: Clone,
           I::Item: Clone
 {
     pub fn as_stream(&self) -> BufferedStream<I> {
         BufferedStream {
             offset: 0,
+            position: unsafe { (*self.buffer.get()).iter.position() },
             buffer: self,
         }
     }
-    fn uncons(&self, offset: usize) -> Result<I::Item, Error<I::Item, I::Item>> {
+    fn uncons(&self, offset: usize) -> Result<(I::Item, I::Position), Error<I::Item, I::Item>> {
         unsafe { (*self.buffer.get()).uncons(offset) }
     }
 }
 
 #[cfg(feature = "buffered_stream")]
-impl<'a, I> BufferedStream<'a, I> where I: Iterator
+impl<'a, I> BufferedStream<'a, I> where I: Iterator + HasPosition
 {
     pub fn new(iter: I, lookahead: usize) -> SharedBufferedStream<I> {
         SharedBufferedStream {
@@ -1076,23 +1083,25 @@ impl<'a, I> BufferedStream<'a, I> where I: Iterator
 #[cfg(feature = "buffered_stream")]
 impl<'a, I> Stream for BufferedStream<'a, I>
     where I: Iterator + HasPosition + 'a,
+          I::Position: Clone,
           I::Item: Clone + PartialEq + 'a
 {
     type Item = I::Item;
     type Range = I::Item;
 
     fn uncons(mut self) -> Result<(I::Item, BufferedStream<'a, I>), Error<I::Item, I::Item>> {
-        let value = try!(self.buffer.uncons(self.offset));
+        let (value, position) = try!(self.buffer.uncons(self.offset));
+        self.position = position;
         self.offset += 1;
         Ok((value, self))
     }
 }
 
 #[cfg(feature = "buffered_stream")]
-impl<'a, I> HasPosition for BufferedStream<'a, I> where I: Iterator + HasPosition {
+impl<'a, I> HasPosition for BufferedStream<'a, I> where I: Iterator + HasPosition, I::Position: Clone {
     type Position = I::Position;
 
     fn position(&self) -> Self::Position {
-        unsafe { (*self.buffer.buffer.get()).iter.position() }
+        self.position.clone()
     }
 }
