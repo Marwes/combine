a37cbd38ecfafcffff7bcdaa85acc2b8e3f1cb57
Markus Westerlind <marwes91@gmail.com>
2018-02-03 00:19:57 +0100

    docs: Add docs on BufferedStream

--- src/stream/buffered.rs
+++ src/stream/buffered.rs
@@ -3,6 +3,25 @@ use std::collections::VecDeque;
 use error::StreamError;
 use stream::{Positioned, Resetable, StreamErrorFor, StreamOnce};
 
+/// `Stream` which buffers items from an instance of `StreamOnce` into a ring buffer.
+/// Instancs of `StreamOnce` which is not able to implement `Resetable` (such as `ReadStream`) may
+/// use this as a way to implement `Resetable` and become a full `Stream` instance.
+///
+/// The drawback is that the buffer only stores a limited number of items which limits how many
+/// tokens that can be reset and replayed. If a `BufferedStream` is reset past this limit an error
+/// will be returned when `uncons` is next called.
+///
+/// NOTE: If this stream is used in conjunction with an error enhancing stream such as
+/// `easy::Stream` (also via the `easy_parser` method) it is recommended that the `BufferedStream`
+/// instance wraps the `easy::Stream` instance instead of the other way around.
+///
+/// ```ignore
+/// // DO
+/// BufferedStream::new(easy::Stream(..), ..)
+/// // DONT
+/// easy::Stream(BufferedStream::new(.., ..))
+/// parser.easy_parse(BufferedStream::new(..));
+/// ```
 #[derive(Debug, PartialEq)]
 pub struct BufferedStream<I>
 where
