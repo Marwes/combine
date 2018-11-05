1943cd890fbbe3a18fbc3aeee0929e72570f9a37
Markus Westerlind <marwes91@gmail.com>
2018-11-05 21:44:57 +0100

    Try to test no_std buildability´

--- examples/number.rs
+++ examples/number.rs
@@ -1,4 +1,11 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 extern crate combine;
+
+#[cfg(not(feature = "std"))]
+use core::str;
+#[cfg(feature = "std")]
+use std::str;
 
 use combine::error::UnexpectedParse;
 use combine::parser::byte::digit;
@@ -12,11 +19,12 @@ fn main() {
     let mut parser = recognize((
         skip_many1(digit()),
         optional((item(b'.'), skip_many(digit()))),
-    )).and_then(|bs: &[u8]| {
-        let s = unsafe { ::std::str::from_utf8_unchecked(bs) };
+    ))
+    .and_then(|bs: &[u8]| {
+        // `bs` only contains digits which are ascii and thus UTF-8
+        let s = unsafe { str::from_utf8_unchecked(bs) };
         s.parse::<f64>().map_err(|_| UnexpectedParse::Unexpected)
     });
     let result = parser.parse(&b"123.45"[..]);
     assert_eq!(result, Ok((123.45, &b""[..])));
-    println!("{:?}", result);
 }
