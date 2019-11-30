1fe1ff26f6972c4e3ff3b7f671e2d52b3d589b57
Markus Westerlind <marwes91@gmail.com>
2019-11-30 23:16:26 +0100

    Minimum version is now 1.39 to support async decoding

--- travis.sh
+++ travis.sh
@@ -1,15 +1,9 @@
 #!/bin/bash -x
 set -ex
 
-if [[ ${TRAVIS_RUST_VERSION} = "1.33.0" ]]; then
-    ALL_FEATURES="--features rust-1-33"
-else
-    ALL_FEATURES="--all-features"
-fi
-
 cargo build
-cargo test $ALL_FEATURES
-cargo test $ALL_FEATURES --examples
+cargo test --all-features
+cargo test --all-features --examples
 
 cargo test --bench json --bench http -- --test
 cargo check --bench mp4 --features mp4
