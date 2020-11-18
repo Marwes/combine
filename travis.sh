e0bf1af5346723fe594ff4bd516595dbc48231fb
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-11-18 12:06:22 +0100

    chore: Only check the build on 1.40
    Since async_std and tokio 0.3 does not build on 1.40 we can run tests
    there anymore

--- travis.sh
+++ travis.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 set -ex
 
-cargo "$@" build
-cargo "$@" test --all-features
-cargo "$@" test --all-features --examples
+if [[ "$TRAVIS_RUST_VERSION" == "1.40.0" ]]; then
+    cargo "$@" check
+    cargo "$@" check --no-default-features
+else
+    cargo "$@" build
+    cargo "$@" test --all-features
+    cargo "$@" test --all-features --examples
 
-cargo "$@" test --bench json --bench http -- --test
-cargo "$@" check --bench mp4 --features mp4
+    cargo "$@" test --bench json --bench http -- --test
+    cargo "$@" check --bench mp4 --features mp4
 
-cargo "$@" build --no-default-features
-cargo "$@" test --no-default-features --examples
+    cargo "$@" build --no-default-features
+    cargo "$@" test --no-default-features --examples
+fi
