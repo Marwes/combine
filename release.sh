a81ad677d7a9408f4702132b1a62fa0f5187ae93
Markus Westerlind <markus.westerlind@imperva.com>
2026-08-21 16:50:17 +0200

    chore: Update release script

--- release.sh
+++ release.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 VERSION=$1
-if [ -z "$VERSION" ]; then
-    echo "Expected patch, minor or major"
-    exit 1
-fi
 
-clog --$VERSION && \
+git cliff --unreleased --tag $VERSION --prepend CHANGELOG.md && \
     git add CHANGELOG.md && \
     git commit -m "Updated changelog" && \
     cargo release --execute $VERSION
